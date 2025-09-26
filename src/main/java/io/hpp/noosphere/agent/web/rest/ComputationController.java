3d7a9db1036c5afc4e97581e0746fb7263f26bc4
jjang <yoonjoo.jang@gmail.com>
2025-09-26 16:57:10 +0900

    #3 Refactor computation handling and data model
    - Remove `RequestUnionVM` and associated logic.
    - Simplify request handling by splitting into `OffchainRequestDTO` and `DelegatedRequestDTO`.
    - Update `ComputationController` with separate endpoints for offchain and delegated requests.
    - Enhance batch processing capabilities for offchain and delegated computations.
    - Adjust `RequestValidatorService` to validate individual request types.
    - Cleanup unused imports and streamline helper methods.

--- src/main/java/io/hpp/noosphere/agent/web/rest/ComputationController.java
+++ src/main/java/io/hpp/noosphere/agent/web/rest/ComputationController.java
@@ -8,20 +8,17 @@ import io.hpp.noosphere.agent.service.dto.BaseRequestDTO;
 import io.hpp.noosphere.agent.service.dto.DelegatedRequestDTO;
 import io.hpp.noosphere.agent.service.dto.OffchainRequestDTO;
 import io.hpp.noosphere.agent.service.dto.enumeration.RequestType;
-import io.hpp.noosphere.agent.web.rest.vm.RequestUnionVM;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.ReactiveUserDetailsPasswordService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/computations")
 public class ComputationController {
 
     private static final Logger log = LoggerFactory.getLogger(ComputationController.class);
@@ -40,19 +37,121 @@ public class ComputationController {
         this.dataStoreService = dataStoreService;
     }
 
-    @PostMapping("/computations")
-    public CompletableFuture<ResponseEntity<Map<String, Object>>> createComputations(
-        @RequestBody RequestUnionVM requestData,
+    @PostMapping("/offchain")
+    public CompletableFuture<ResponseEntity<Map<String, Object>>> createOffchainComputation(
+        @RequestBody OffchainRequestDTO requestData,
         HttpServletRequest request
     ) {
         return handleComputationRequest(requestData, request, false);
     }
 
-    @GetMapping("/computations")
+    @PostMapping("/delegated")
+    public CompletableFuture<ResponseEntity<Map<String, Object>>> createDelegatedComputation(
+        @RequestBody DelegatedRequestDTO requestData,
+        HttpServletRequest request
+    ) {
+        return handleComputationRequest(requestData, request, false);
+    }
+
+    @PostMapping("/offchain/batch")
+    public CompletableFuture<ResponseEntity<List<Map<String, Object>>>> createOffchainBatch(
+        @RequestBody List<OffchainRequestDTO> requestDataList,
+        HttpServletRequest request
+    ) {
+        String clientIp = getClientIp(request);
+        if (clientIp == null) {
+            return CompletableFuture.completedFuture(
+                ResponseEntity.badRequest().body(List.of(Map.of("error", "Could not get client IP address")))
+            );
+        }
+
+        try {
+            log.debug("Received new off-chain raw message batch: {}", requestDataList);
+            List<OffchainRequestDTO> validRequestList = requestDataList
+                .stream()
+                .filter(requestValidatorService::validateOffChainRequest)
+                .toList();
+
+            List<Map<String, Object>> results = new ArrayList<>();
+            List<CompletableFuture<Void>> futures = new ArrayList<>();
+
+            for (OffchainRequestDTO item : validRequestList) {
+                item.setId(UUID.randomUUID());
+                item.setClientIp(clientIp);
+                item.setType(RequestType.OFF_CHAIN_COMPUTATION);
+                futures.add(computationService.processOffchainComputation(item));
+                results.add(Map.of("id", item.getId()));
+            }
+
+            return CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).thenApply(v -> {
+                log.debug(
+                    "Processed REST response - endpoint: /api/computations/offchain/batch, " + "method: POST, status: 200, results: {}",
+                    results
+                );
+                return ResponseEntity.ok(results);
+            });
+        } catch (Exception e) {
+            log.error("Error processing batch Computations", e);
+            return CompletableFuture.completedFuture(
+                ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                    List.of(Map.of("error", "Could not enqueue Computation: " + e.getMessage()))
+                )
+            );
+        }
+    }
+
+    @PostMapping("/delegated/batch")
+    public CompletableFuture<ResponseEntity<List<Map<String, Object>>>> createDelegatedBatch(
+        @RequestBody List<DelegatedRequestDTO> requestDataList,
+        HttpServletRequest request
+    ) {
+        String clientIp = getClientIp(request);
+        if (clientIp == null) {
+            return CompletableFuture.completedFuture(
+                ResponseEntity.badRequest().body(List.of(Map.of("error", "Could not get client IP address")))
+            );
+        }
+
+        try {
+            log.debug("Received new off-chain raw message batch: {}", requestDataList);
+            List<DelegatedRequestDTO> validRequestList = requestDataList
+                .stream()
+                .filter(requestValidatorService::validateDelegatedRequest)
+                .toList();
+
+            List<Map<String, Object>> results = new ArrayList<>();
+            List<CompletableFuture<Void>> futures = new ArrayList<>();
+
+            // TODO: delegated bach process
+            //            for (DelegatedRequestDTO item : validRequestList) {
+            //                item.setId(UUID.randomUUID());
+            //                item.setClientIp(clientIp);
+            //                item.setType(RequestType.OFF_CHAIN_COMPUTATION);
+            //                futures.add(computationService.processOffchainComputation(item));
+            //                results.add(Map.of("id", item.getId()));
+            //            }
+
+            return CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).thenApply(v -> {
+                log.debug(
+                    "Processed REST response - endpoint: /api/computations/delegated/batch, " + "method: POST, status: 200, results: {}",
+                    results
+                );
+                return ResponseEntity.ok(results);
+            });
+        } catch (Exception e) {
+            log.error("Error processing batch Computations", e);
+            return CompletableFuture.completedFuture(
+                ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                    List.of(Map.of("error", "Could not enqueue Computation: " + e.getMessage()))
+                )
+            );
+        }
+    }
+
+    @GetMapping
     public ResponseEntity<Object> getComputations(
         @RequestParam(value = "id", required = false) List<UUID> computationIds,
         @RequestParam(value = "pending", required = false) String pending,
-        @RequestParam(value = "intermediate", required = false, defaultValue = "false") boolean intermediate,
         HttpServletRequest request
     ) {
         String clientIp = getClientIp(request);
@@ -91,7 +190,7 @@ public class ComputationController {
 
     // Helper methods
     private CompletableFuture<ResponseEntity<Map<String, Object>>> handleComputationRequest(
-        RequestUnionVM requestData,
+        BaseRequestDTO requestData,
         HttpServletRequest request,
         boolean isStream
     ) {
@@ -102,8 +201,7 @@ public class ComputationController {
 
         try {
             Map<String, Object> returnObj = new HashMap<>();
-            if (requestData.isOffchainRequest()) {
-                OffchainRequestDTO requestDTO = requestData.getOffchainRequest();
+            if (requestData instanceof OffchainRequestDTO requestDTO && !isStream) {
                 requestDTO.setId(UUID.randomUUID());
                 requestDTO.setClientIp(clientIp);
                 requestDTO.setType(RequestType.OFF_CHAIN_COMPUTATION);
@@ -121,8 +219,7 @@ public class ComputationController {
                         );
                         return ResponseEntity.ok(returnObj);
                     });
-            } else if (requestData.isDelegatedRequest()) {
-                DelegatedRequestDTO requestDTO = requestData.getDelegatedRequest();
+            } else if (requestData instanceof DelegatedRequestDTO requestDTO && !isStream) {
                 // TODO: delegated call section
                 //                return chainManagerService.procassDelegated(requestDTO)
                 //                    .thenApply(v -> {
@@ -133,11 +230,11 @@ public class ComputationController {
                 //                        return ResponseEntity.ok(returnObj);
                 //                    });
             } else {
-                throw new IllegalArgumentException("Unknown message type: " + requestData.getRequestType());
+                throw new IllegalArgumentException("Unknown request type: " + requestData.toString());
             }
 
             return CompletableFuture.completedFuture(
-                ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", "Unknown message type"))
+                ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", "Unknown request type"))
             );
         } catch (Exception e) {
             log.error("Error processing Computation request", e);
