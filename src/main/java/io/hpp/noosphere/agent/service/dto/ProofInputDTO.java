13a507cf47745c02ecec05f6f0765c5a8eee16e3
jjang <yoonjoo.jang@gmail.com>
2025-11-13 16:31:45 +0900

    #17 Introduce `InlineDataDTO` and `ProofInputDTO` for structured proof data handling in DTOs.

---
+++ src/main/java/io/hpp/noosphere/agent/service/dto/ProofInputDTO.java
@@ -0,0 +1,15 @@
+package io.hpp.noosphere.agent.service.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ProofInputDTO {
+
+    private String requestId;
+    private InlineDataDTO commitment;
+    private InlineDataDTO input;
+    private InlineDataDTO output;
+    private long timestamp;
+}
